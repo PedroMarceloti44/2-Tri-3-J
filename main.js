d5520918514a28f8b83fdc8836c409f8b2a2c56b
PedroMarceloti44 <psoaress044@gmail.com>
2024-06-14 09:42:29 -0300

    14/06/2024 :0

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const botoes  = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".abas-conteudo");
-const tempo = document.querySelectorAll("contador");
+const tempo = document.querySelectorAll(".contador");
+tempo[0].textContent="Ola"
+const tempoObjetivo = new Date(2024,11,24,23,59);
+const tempoObjetivo2 = new Date();
+
+const agora = new Date();
+tempo[1].textContent=agora
 console.log(tempo);
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function(){
