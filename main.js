786f4d57785dbe4c98cdc25d0c6ee111cfe38a6d
PedroMarceloti44 <psoaress044@gmail.com>
2024-07-05 09:47:44 -0300

    05/07/2024

--- main.js
+++ main.js
@@ -1,29 +1,39 @@
 const botoes  = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".abas-conteudo");
 const tempo = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date(2024,11,24,23,59);
-const tempoObjetivo2 = new Date(2024,9,28,23,59);
-const tempoObjetivo3 = new Date(2025,0,1,23,59);
-const tempoObjetivo4 = new Date(2025,2,14,23,59);
-const agora = new Date();
-let segundos = Math.floor((tempoObjetivo1-agora)/1000)
-let minutos = Math.floor(segundos/60)
-let horas = Math.floor(minutos/60)
-let dias = Math.floor(horas/24)
 
+const tempoObjetivo1 = new Date(2030,11,24,23,59);
+const tempoObjetivo2 = new Date(2030,9,28,23,59);
+const tempoObjetivo3 = new Date(2030,0,1,23,59);
+const tempoObjetivo4 = new Date(2030,2,14,23,59);
+function calculaTempo(tempoObjetivo){
+const agora = new Date();
+let segundos 
+let minutos
+let horas
+let dias
+segundos = Math.floor((tempoObjetivo-agora)/1000)
+minutos = Math.floor(segundos/60)
+horas = Math.floor(minutos/60)
+dias = Math.floor(horas/24)
 segundos = segundos%60
 minutos = minutos%60
 horas = horas%24
 
-tempo[0].textContent = `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
-console.log(tempo);
+return `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
+}
+tempo[0].textContent = calculaTempo(tempoObjetivo1);
+tempo[1].textContent = calculaTempo(tempoObjetivo2);
+tempo[2].textContent = calculaTempo(tempoObjetivo3);
+tempo[3].textContent = calculaTempo(tempoObjetivo4);
+
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function(){
         for (let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
             conteudo[j].classList.remove("ativo")
         }
-        botoes[i].classList.add("ativo ")
+        botoes[i].classList.add("ativo")
         conteudo[i].classList.add("ativo")
     }
 }
