3dca7337404101e43301bd9502dfb472d01a8bca
PedroMarceloti44 <psoaress044@gmail.com>
2024-07-25 08:03:03 -0300

    25/07/24

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ const tempo = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date(2030,11,24,23,59);
 const tempoObjetivo2 = new Date(2030,9,28,23,59);
 const tempoObjetivo3 = new Date(2030,0,1,23,59);
-const tempoObjetivo4 = new Date(2030,2,14,23,59);
+const tempoObjetivo4 = new Date(2023,2,14,23,59);
+
 function calculaTempo(tempoObjetivo){
 const agora = new Date();
 let segundos 
@@ -16,11 +17,16 @@ segundos = Math.floor((tempoObjetivo-agora)/1000)
 minutos = Math.floor(segundos/60)
 horas = Math.floor(minutos/60)
 dias = Math.floor(horas/24)
+
 segundos = segundos%60
 minutos = minutos%60
 horas = horas%24
 
-return `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
+if(segundos >=0){
+   return `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
+} else{
+    return 'Prazo finalizado!'
+ }
 }
 tempo[0].textContent = calculaTempo(tempoObjetivo1);
 tempo[1].textContent = calculaTempo(tempoObjetivo2);
